1725dea28e6ac283bb53e0379eb6385d8ef157e1
Alex Suatac <alexsuatac@gmail.com>
2021-12-13 22:05:01 -0500

    prime number finder

--- Problem3/Problem3.cpp
+++ Problem3/Problem3.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
-#oinm
 
-int main (int argc, char *argv[]) {
-    
-    bool goodArg = (argc == 2) && (std::stoi(argv[1]) > 0);
-    if (!goodArg) {
-        std::cout << "Usage: p3 <positive integer>" << std::endl;\
-        return 0;
+bool isPrime(int n) {
+    if (n == 0 || n == 1) {
+        return false;
     }
 
+    for (int i = 2; i < n/2; ++i) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
 
+    return true;
+}
+
+int main () {
+
+    // make function that determines if number is prime
+
+    // main loop will count down from value, checking if each number is prime
+    // when it is, it will be the largest one
+     
     return 0;
 }
