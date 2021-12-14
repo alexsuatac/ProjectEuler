dd33c3ffac25000f9cf783b693bc364674bd1d81
Alex Suatac <alexsuatac@gmail.com>
2021-12-13 22:28:59 -0500

    find largest prime factor with loop

--- Problem3/Problem3.cpp
+++ Problem3/Problem3.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
-bool isPrime(int n) {
+bool isPrime(long n) {
     if (n == 0 || n == 1) {
         return false;
     }
 
-    for (int i = 2; i < n/2; ++i) {
+    for (long i = 2; i < n/2; ++i) {
         if (n % i == 0) {
             return false;
         }
@@ -16,10 +16,19 @@ bool isPrime(int n) {
 
 int main () {
 
-    // make function that determines if number is prime
+    const static long long VAL = 600851475143;
+    
+    for (long i = VAL/2; i > 0; --i) { // a number can't be evenly divisible by more than 1/2 of itself
+        // find if i is a factor of VAL
+        if (VAL % i == 0) {
+            if (isPrime(i)) {
+                std::cout << "Largest prime factor of "
+                << VAL << " is " << i << std::endl;
+                return 0;
+            }
+        }
+    }
 
-    // main loop will count down from value, checking if each number is prime
-    // when it is, it will be the largest one
-     
+    std::cout << "There are no prime factors" << std::endl;
     return 0;
 }
